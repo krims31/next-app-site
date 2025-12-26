2426b5cb2fe7ce70fa6b7032d2ae41bb190b669b
krims31 <vegor405@gmail.com>
2025-12-26 12:07:46 +0300

    fix

--- app/components/Article.tsx
+++ app/components/Article.tsx
@@ -1,19 +1,27 @@
+import { article } from './data/article'
+import img1 from "./assets/img1.png"
 export default function Article() {
 	return (
 		<main>
-			<h1 className="text-5xl font-bold mt-20 ml-20">Article Post</h1>
-			<p className="text-gray-400 ml-20 mt-5">
-				Get started today and take your reading experience wherever you go!
-			</p>
+			<h1 className="text-5xl font-bold mt-20 ml-20">{article.post}</h1>
+			<p className="text-gray-400 ml-20 mt-5">{article.getStarted}</p>
 			<div className="flex gap-5 mt-5 ml-20">
 				<button className="border-gray-200 rounded-3xl bg-gray-200 font-bold p-2 px-5 cursor-pointer">
-					All
+					{article.all}
 				</button>
-				<button className="text-gray-400">Newest</button>
-				<button className="text-gray-400">Popular</button>
-				<button className="text-gray-400">Work</button>
-				<button className="text-gray-400">Machine Learning</button>
-				<button className="text-gray-400">Artificial Intelligence</button>
+				<button className="text-gray-400">{article.new}</button>
+				<button className="text-gray-400">{article.popular}</button>
+				<button className="text-gray-400">{article.work}</button>
+				<button className="text-gray-400">{article.ml}</button>
+				<button className="text-gray-400">{article.ai}</button>
+				<img
+					src={img1}
+					alt="blockchain"
+				/>
+				<p>{article.jan}</p>
+				<p>{article.python}</p>
+				<h1>{article.block}</h1>
+				<p>{article.learn}</p>
 			</div>
 		</main>
 	)
---
+++ app/components/data/article.ts
@@ -0,0 +1,30 @@
+interface Art {
+	post: string
+	getStarted: string
+	all: string
+	new: string
+	popular: string
+	work: string
+	ml: string
+	ai: string
+	jan: string
+	python: string
+	block: string
+	learn: string
+}
+
+export const article = {
+	post: 'Article Post',
+	getStarted:
+		'Get started today and take your reading experience wherever you go!',
+	all: 'All',
+	new: 'Newest',
+	popular: 'Popular',
+	work: 'Work',
+	ml: 'Machine Learning',
+	ai: 'Artificial Intelligence',
+	jan: 'Jan 7, 2024',
+	python: 'Python',
+	block: 'Python for Blockchain: A Quick Guide',
+	learn: 'Learn how Python simplifies blockchain development.'
+} as Art
