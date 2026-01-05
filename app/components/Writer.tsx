0b8c342d268a53a98e3aa3260b8b7c98cd5a39ce
krims31 <vegor405@gmail.com>
2026-01-05 14:25:57 +0300

    add new files

--- app/components/Writer.tsx
+++ app/components/Writer.tsx
@@ -3,10 +3,88 @@ import { writer } from "./data/writer.ts"
 export default function Writer() {
   return (
     <>
-    <div>
-      <h1>{writer.write}</h1>
-    </div>
+      <div>
+        <h1 className="ml-330 mt-15 font-bold">{writer.write}</h1>
+        
+        <div className="flex items-center ml-330 mt-5">
+          <div className="flex items-center space-x-4">
+            <img 
+              src={writer.profileGuy} 
+              alt="guy" 
+              className="rounded-3xl"
+              width={50} 
+              height={50}
+            />
+            <div>
+              <h1 className="font-bold">{writer.inspWriter}</h1>
+              <p className="text-gray-400 mt-1">{writer.dog}</p>
+            </div>
+          </div>
+          <button className="border border-gray-300 rounded-2xl px-6 py-2 bg-black text-white ml-4">
+            {writer.follow}
+          </button>
+        </div>
+
+        <div className="flex items-center ml-330 mt-5">
+          <div className="flex items-center space-x-4">
+            <img 
+              src={writer.cameronImg} 
+              alt="cameron" 
+              className="rounded-3xl" 
+              width={50} 
+              height={50}
+            />
+            <div>
+              <h1 className="font-bold">{writer.cameron}</h1>
+              <p className="text-gray-400 mt-1">{writer.medicial}</p>
+            </div>
+          </div>
+          <button className="border border-gray-300 rounded-2xl px-6 py-2 bg-black text-white ml-4">
+            {writer.follow}
+          </button>
+        </div>
+
+        <div className="flex items-center ml-330 mt-5">
+          <div className="flex items-center space-x-4">
+            <img 
+              src={writer.theresaImg} 
+              alt="theresa" 
+              className="rounded-3xl" 
+              width={50} 
+              height={50}
+            />
+            <div>
+              <h1 className="font-bold">{writer.theresa}</h1>
+              <p className="text-gray-400 mt-1">{writer.president}</p>
+            </div>
+          </div>
+          <button className="border border-gray-300 rounded-2xl px-6 py-2 bg-black text-white ml-4">
+            {writer.follow}
+          </button>
+        </div>
+      
+        <div className="flex items-center ml-330 mt-5">
+          <div className="flex items-center space-x-4">
+            <img 
+              src={writer.estherImg} 
+              alt="esther" 
+              className="rounded-3xl" 
+              width={50} 
+              height={50}
+            />
+            <div>
+              <h1 className="font-bold">{writer.esther}</h1>
+              <p className="text-gray-400 mt-1">{writer.nursing}</p>
+            </div>
+          </div>
+          <button className="border border-gray-300 rounded-2xl px-6 py-2 bg-black text-white ml-4">
+            {writer.follow}
+          </button>
+        </div>
+        <div>
+          <h1 className="ml-330 mt-10 text-amber-500">{writer.seeSug}</h1>
+        </div>
+      </div>
     </>
   )
 }
-
--- app/components/data/writer.ts
+++ app/components/data/writer.ts
@@ -9,10 +9,16 @@ interface Writer {
   nursing: string;
   theresa: string;
   esther: string;
+  follow: string;
+  profileGuy: string;
+  cameronImg: string;
+  theresaImg: string;
+  estherImg: string;
+  seeSug: string;
 }
 
 export const writer = {
-  write: "Inspited Writer",
+  write: "Inspired Writer",
   inspWriter: "Guy Hawkins",
   dog: "Dog Trainer",
   cameron: "Cameron Williamson",
@@ -21,4 +27,10 @@ export const writer = {
   president: "President of Sales",
   esther: "Esther Howard",
   nursing: "Nursing Assistant",
+  follow: "Follow",
+  profileGuy: "https://img.freepik.com/premium-photo/png-smiling-man-with-beard-round-badge-transparent-background_53876-953494.jpg?semt=ais_hybrid&w=740&q=80",
+  cameronImg: "https://img.freepik.com/premium-photo/png-circle-profile-picture-sticker-business-woman-transparent-background_53876-945863.jpg?semt=ais_hybrid&w=740&q=80",
+  theresaImg: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEDU_TmMK9xYGD2Vhk7IkYO-9g5VOi4QHG0A&s",
+  estherImg: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpX_BZoIhTfK3nl8EcpmqfMQd9LdyC9qpszA&s",
+  seeSug: "See more Suggestion",
 } as Writer;
